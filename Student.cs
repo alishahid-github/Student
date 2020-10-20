2d68a1624cc55aef9cab7604cf9feaa3204a4c61
Ali Shahid <71405228+alishahid-github@users.noreply.github.com>
2020-10-20 14:25:20 +0500

    Update Student.cs

--- Student.cs
+++ Student.cs
@@ -1,5 +1,5 @@
+
 using System;
-using System.Collections.Generic;
 
 namespace Student
 {
@@ -7,9 +7,9 @@ namespace Student
     {
         public Student()       //Default Constructor
         {
-            this.studentName = "\0";
-            this.registrationNumber = "-0";
-            this.CNIC = "-0";
+            this.studentName = "Not Initalized";
+            this.registrationNumber = "Not Initalized";
+            this.CNIC = "Not Initalized";
             this.dateOfBirth = DateTime.MinValue;
             this.CGPA = 0.0;
             this.hobbies = null;
@@ -19,6 +19,10 @@ namespace Student
         {
             this.studentName = name;
             this.registrationNumber = number;
+            this.CNIC = "Not Initalized";
+            this.dateOfBirth = DateTime.MinValue;
+            this.CGPA = 0.0;
+            this.hobbies = null;
         }
 
         //data memebers | properties of 6 data Memebers
@@ -29,29 +33,23 @@ namespace Student
         public double CGPA { get; set; }
         public string[] hobbies { get; set; }
 
-        public bool Input()         // method for taking input from users and save into the object
+        //functions for validation
+        private bool validate_name(string name)
         {
-            string name, rNumber, cnic;
-            DateTime d;
-            double cgpa;
-            string[] hobbies;
-
-            Console.Write("Enter Name of Student: ");
-            name = Console.ReadLine();
-
             foreach (var item in name)      //checking the validation for name
             {
-                if (!((item >= 'A' && item <= 'Z') || (item >= 'a' && item <= 'z') || (item ==' ')))
+                if (!((item >= 'A' && item <= 'Z') || (item >= 'a' && item <= 'z') || (item == ' ')))
                 {
                     Console.WriteLine("Name should be alphabetic. Speciall charecter or numbers are not allowed");
                     return false;
                 }
 
             }
+            return true;
+        }
 
-            Console.Write("Enter Registration Number of Student: ");
-            rNumber = Console.ReadLine();
-            
+        private bool validate_number(string rNumber)
+        {
 
             if (rNumber.Length > 11)    //checking the validation for registration number
             {
@@ -59,8 +57,7 @@ namespace Student
                 return false;
             }
 
-
-            if(rNumber.Length == 11)               //checking the validation for registration number
+            if (rNumber.Length == 11)               //checking the validation for registration number
             {
                 for (int i = 0; i < rNumber.Length; i++)
                 {
@@ -83,8 +80,11 @@ namespace Student
                 return false;
             }
 
-            Console.Write("Enter CNIC of Student: ");
-            cnic = Console.ReadLine();
+            return true;
+        }
+
+        private bool validate_cnic(string cnic)
+        {
 
             if (cnic.Length != 13)              //checking the validation for CNIC
             {
@@ -101,43 +101,100 @@ namespace Student
                 }
 
             }
+            return true;
+        }
 
-            Console.Write("Enter Student Date of Birth(mm/dd/yyyy): ");
-            d = Convert.ToDateTime(Console.ReadLine());
-
+        private bool validate_dob(DateTime d)
+        {
             if (!(d.Year > 1990 && d.Year < 2005))              //checking the validation for DOB
             {
                 Console.WriteLine("Date of birth should be less than 1st January 2005 and greater than 31st December 1990");
                 return false;
             }
-            else if (d.Month == 2)
-            {
-                if (d.Day > 29)
-                {
-                    Console.WriteLine("Day must must be between 1-29 for February");
-                    return false;
-                }
-            }
-            else if (!(d.Month >= 1 && d.Month <= 12))
-            {
-                Console.WriteLine("Month must be between 1-12");
-                return false;
-            }
 
-            else if (!(d.Day >= 1 && d.Day <= 31))
-            {
-                Console.WriteLine("Month must be between 1-12");
-                return false;
-            }
+            return true;
 
-            Console.Write("Enter CGPA of Student: ");               
-            cgpa = Convert.ToDouble(Console.ReadLine());
+        }
 
-            if (!(cgpa >= 0 && cgpa <= 4))                  //checking the validation for CGPA
+        private bool validate_cgpa(double cgpa)
+        {
+            if (!(cgpa >= 0.0 && cgpa <= 4.0))                  //checking the validation for CGPA
             {
                 Console.WriteLine("cgpa must be between 0-4");
                 return false;
             }
+            return true;
+        }
+        public void Input()           // method for taking input from users and save into the object
+        {
+            string name, rNumber, cnic;
+            DateTime dob;
+            double cgpa;
+            string[] hobbies;
+            bool correct;       //loop for taking again input untill correct is not found
+            do
+            {
+                Console.Write("Enter Name of Student: ");
+                name = Console.ReadLine();
+                correct= validate_name(name);
+                if (!correct)
+                    Console.WriteLine("Invalid Student Name");
+            } while (!correct);
+
+            this.studentName = name;
+
+            do
+            {
+                Console.Write("Enter Registration Number of Student: ");
+                rNumber = Console.ReadLine(); 
+                correct = validate_number(rNumber);
+                if (!correct)
+                    Console.WriteLine("Invalid Registration NUmber");
+            } while (!correct);
+
+            this.registrationNumber = rNumber;
+
+            do
+            {
+                Console.Write("Enter CNIC of Student: ");
+                cnic = Console.ReadLine(); 
+                correct = validate_cnic(cnic);
+                if (!correct)
+                    Console.WriteLine("Invalid CNIC ");
+            } while (!correct);
+
+            this.CNIC = cnic;
+
+            do
+            {
+                Console.Write("Enter Student Date of Birth(mm/dd/yyyy): ");
+                try
+                {
+                    dob = Convert.ToDateTime(Console.ReadLine());
+                    correct = validate_dob(dob);
+                    if (!correct)
+                        Console.WriteLine("Invalid Date Of Birth");
+                    else
+                        this.dateOfBirth = dob;
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("Invalid date");
+                    correct = false;
+                }         
+               
+            } while (!correct);
+            
+
+            do
+            {
+                Console.Write("Enter CGPA of Student: ");
+                cgpa = Convert.ToDouble(Console.ReadLine());
+                correct = validate_cgpa(cgpa);
+                if (!correct)
+                    Console.WriteLine("Invalid CGPA");
+            } while (!correct);
+            this.CGPA = cgpa;
 
             Console.Write("How many hobbies you want to enter: ");
             int n = Convert.ToInt32(Console.ReadLine());
@@ -149,18 +206,11 @@ namespace Student
                 hobbies[i] = Console.ReadLine();
 
             }
-
             this.hobbies = hobbies;
-            this.registrationNumber = rNumber;
-            this.CNIC = cnic;
-            this.studentName = name;
-            this.CGPA = cgpa;
-            this.dateOfBirth = d;
 
             Console.WriteLine("You input has successfully inserted!" + Environment.NewLine);
             Console.WriteLine();
-            return true;
-
+            
         }
 
         public string Get_Age()     //this function will calculate the age
@@ -202,7 +252,7 @@ namespace Student
         {
             if (Convert.ToInt32(this.CNIC[CNIC.Length - 1]) % 2 == 0)
                 return "Female";
-            else
+            else 
                 return "Male";
         }
 
@@ -231,21 +281,28 @@ namespace Student
 
         public void display()           //function to display the data
         {
-            Console.WriteLine("Initialized data is: \n Name: {0} \n" +
-                                         "Registration Number: {1}\n" +
-                                         "CGPA: {2}\n" +
-                                         "Date of Birth: {3} \n" +
-                                         "CNIC: {4}\n" +
-                                         "Gender: {5}\n"
-                                         , this.studentName, this.registrationNumber, this.CGPA
-                                         , this.dateOfBirth.ToString("MMMM dd,  yyyy"), this.CNIC, this.Get_Gender());
-
-            Console.Write("Hobbies: \n");
-
-            foreach (var item in hobbies)
+            try
             {
-                Console.WriteLine(item);
+                Console.WriteLine("Initialized data is: \nName: {0} \n" +
+                                        "Registration Number: {1}\n" +
+                                        "CGPA: {2}\n" +
+                                        "Date of Birth: {3} \n" +
+                                        "CNIC: {4}\n" 
+                                        , this.studentName, this.registrationNumber, this.CGPA
+                                        , this.dateOfBirth.ToString("MMMM dd,  yyyy"), this.CNIC);
+
+                Console.Write("Hobbies: ");
+
+                foreach (var item in hobbies)
+                {
+                    Console.WriteLine(item);
+                }
             }
+            catch (Exception)
+            {
+                Console.WriteLine("Not intialized");
+            }
+           
         }
         ~Student()          //destructor
         {
